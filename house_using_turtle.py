a849a07c793b14694e18aa5e6187319b5ef5ff8c
Paula Onea <paula.onea@gmail.com>
2019-10-27 21:16:29 +0000

    code refactoring

--- house_using_turtle.py
+++ house_using_turtle.py
@@ -21,29 +21,40 @@ def window(x, y):
     position(x, y)
     turtle.seth(90)
     rectangle(6, 75)
-    position(x+8, y+6)
+    position(x + 8, y + 6)
     turtle.seth(90)
     rectangle(60, 60)
-    position(x+8, y+36)
+    position(x + 8, y + 36)
     turtle.seth(0)
     turtle.forward(60)
-    position(x+38, y+6)
+    position(x + 38, y + 6)
     turtle.seth(90)
     turtle.forward(60)
-    position(x+68, y+66)
-    turtle.circle(30,180)
+    position(x + 68, y + 66)
+    turtle.circle(30, 180)
 
 
-def door(x,y):
+def door(x, y):
     position(x, y)
     turtle.seth(90)
     rectangle(120, 60)
-    position(x+6, y+6)
+    position(x + 6, y + 6)
     turtle.seth(90)
     rectangle(52, 48)
-    position(x+6, y+60)
+    position(x + 6, y + 61)
     turtle.seth(90)
     rectangle(52, 48)
+    position(x + 60, y + 120)
+    turtle.circle(30, 180)
+    position(x + 28, y + 120)
+    turtle.seth(135)
+    turtle.forward(28)
+    position(x + 30, y + 120)
+    turtle.seth(90)
+    turtle.forward(30)
+    position(x + 32, y + 120)
+    turtle.seth(45)
+    turtle.forward(28)
 
 
 turtle.speed("fastest")
@@ -51,8 +62,7 @@ turtle.speed("fastest")
 for x, y in [(-180, -300), (-180, -120), (-180, 60)]:
     position(x, y)
     turtle.seth(90)
-    rectangle(8,360)
-
+    rectangle(8, 360)
 
 for x, y in [(-174, -292), (-174, - 112), (174, -292), (174, -112)]:
     turtle.penup()
@@ -61,14 +71,11 @@ for x, y in [(-174, -292), (-174, - 112), (174, -292), (174, -112)]:
     turtle.seth(90)
     turtle.forward(172)
 
-
 window(-146, -80)
 window(-37, -80)
 window(72, -80)
-window(-146, -250)
-window(72, -250)
-door(-37, -290)
-
-
+window(-146, -240)
+window(72, -240)
+door(-37, -288)
 
 turtle.done()
