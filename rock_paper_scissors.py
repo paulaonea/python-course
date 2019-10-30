a16b6418d47ce8ca505d1cc412609cce075ef645
Paula Onea <paula.onea@gmail.com>
2019-10-30 10:38:10 +0000

    Added choice validation

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,17 +2,32 @@
 # you can choose to play against a friend or against the computer.
 # if you choose to play against the computer, the computer's choice will be generated randomly.
 
-
 import random
 
+
 def random_choice():
-    choices = ['rock', 'paper', 'scissors']
     return choices[random.randint(0, 2)]
 
 
+def validation(choice):
+    if choice in choices:
+        return choice
+    else:
+        return 'error'
+
+
+def choice(name):
+    choice = input(f'{name}, please choose rock, scissors or paper: ')
+    while validation(choice) == 'error':
+        choice = input(f'{name}, your choice is not valid, please choose again (rock, paper, scissors: ')
+    return choice
+
+
 def result(choice1, choice2):
-    winners = [('rock', 'scissors'), ('paper', 'rock'), ('scissors', 'paper')]
-    if (choice1, choice2) in winners:
+    winning_combinations = [('rock', 'scissors'), ('paper', 'rock'), ('scissors', 'paper')]
+    if choice1 == choice2:
+        return 'draw'
+    elif (choice1, choice2) in winning_combinations:
         return 'won'
     else:
         return 'lost'
@@ -20,19 +35,25 @@ def result(choice1, choice2):
 
 your_name = input('What is your name?: ')
 print(f'Hello {your_name}. This is Rock, Paper, Scissors game. You can play against a friend or against the computer.')
-opponent = input(f'Please choose your opponent: (computer / friend) ')
-if opponent.lower() == 'friend':
-    friend_name = input(f'What is your friend\'s name?: ')
+opponent = input(f'Do you want to play against the computer? (y/n) ')
+if opponent == 'n':
+    friend_name = input(f'{your_name}, you chose to play against a friend. What is your friend\'s name?: ')
 
-your_choice = input(f'{your_name}, please choose rock, scissors or paper: ')
-if opponent.lower() == 'friend':
-    opponent_choice = input(f'{friend_name}, please choose rock, scissors or paper: ')
+choices = ['rock', 'paper', 'scissors']
+
+your_choice = choice(your_name)
+
+if opponent == 'n':
+    opponent_choice = choice(friend_name)
 else:
     opponent_choice = random_choice()
     print(f'Your opponent chose {opponent_choice}.')
 
 game_result = result(your_choice, opponent_choice)
-print(f'{your_name} you {game_result}.')
+if game_result == 'draw':
+    print(f'The game resulted in DRAW.')
+else:
+    print(f'{your_name}, you {game_result}!')
 
 
 
