5113d677985a55940d804cbc98db49f324bae448
Paula Onea <paula.onea@gmail.com>
2019-11-03 01:00:00 +0000

    code refactoring

--- rock_paper_scissors_tests.py
+++ rock_paper_scissors_tests.py
@@ -3,25 +3,17 @@ from rock_paper_scissors import result
 
 
 class Test(TestCase):
-    def test_1(self):
-        self.assertEqual(result('rock', 'paper'), "lost")
+    def test_wins(self):
+        win_options = [('paper', 'rock'), ('rock', 'scissors'), ('scissors', 'paper')]
+        for i, j in win_options:
+            self.assertEqual(result(i, j), 'won')
 
-    def test_2(self):
-        self.assertEqual(result('paper', 'rock'), "won")
+    def test_loses(self):
+        lose_options = [('scissors', 'rock'), ('rock', 'paper'), ('paper', 'scissors')]
+        for i, j in lose_options:
+            self.assertEqual(result(i, j), 'lost')
 
-    def test_3(self):
-        self.assertEqual(result('rock', 'scissors'), "won")
-
-    def test_4(self):
-        self.assertEqual(result('scissors', 'rock'), "lost")
-
-    def test_5(self):
-        self.assertEqual(result('scissors', 'paper'), "won")
-
-    def test_6(self):
-        self.assertEqual(result('paper', 'scissors'), "lost")
-
-    def test_7(self):
+    def test_draws(self):
         draw_options = [('rock', 'rock'), ('paper', 'paper'), ('scissors', 'scissors')]
         for i, j in draw_options:
             self.assertEqual(result(i, j), 'draw')
