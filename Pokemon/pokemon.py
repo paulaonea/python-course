b43ed76c0fac525cadc70d0e2c1e294420348322
Paula Onea <paula.onea@gmail.com>
2019-12-22 16:39:00 +0000

    pokemon game

--- Pokemon/pokemon.py
+++ Pokemon/pokemon.py
@@ -1,4 +1,4 @@
-# In this session you used the Pokemon API to retrieve a single Pokemon. I want a program that can retrieve multiple
+# In this session you used the Pokemon API to retrieve a single Pokemon. The program should retrieve multiple
 # Pokemon and save their names and moves to a file. Use a list to store about 6 Pokemon IDs.
 # Then in a for loop call the API to retrieve the data for each Pokemon.
 # Save their names and moves into a file called 'pokemon.txt'
